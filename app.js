c94a07372c0d269578c5214c05ff1c6dc9e60101
cacostam6398 <36524775+cacostam6398@users.noreply.github.com>
2018-12-13 15:04:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ IdentiApp.run(function ($rootScope, $location, $http, CONFIG,ROLES) {
     console.log('https://',location.host);
     // /pruebas_back
     // /rest_diagnostico/public
-    $rootScope.baseUri = 'httsp://'+ location.host + '/pruebas_back' ;
+    $rootScope.baseUri = 'https://'+ location.host + '/pruebas_back' ;
 
 
 
